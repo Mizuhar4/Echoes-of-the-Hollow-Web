970afff37515419a078ab740fb9425a8f85c59d9
Mizuhar4 <b.fernandez07@ufromail.cl>
2025-09-28 20:28:50 -0300

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white p-4">
+      <h1 className="text-3xl font-bold">Echoes of the Hollow Web</h1>
+    </div>
+  );
+}
+
+
+export default App;
